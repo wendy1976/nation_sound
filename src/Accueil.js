46d7a0d1bb3e6fd8b3676cabc7a86601a6879f70
wendy1976 <caroline.ferru@free.fr>
2023-12-01 15:18:01 +0100

    design, countdown

--- src/Accueil.js
+++ src/Accueil.js
@@ -1,71 +1,110 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
+import { Button, Card } from 'react-bootstrap';
 import { MdEmail } from 'react-icons/md';
 import { Link } from 'react-router-dom';
 import ArtistCard from './ArtistCard';
 import CarouselComponent from './CarouselComponent';
+import FestivalCountdown from './FestivalCountdown';
 import Footer from './Footer';
 import Header from './Header';
 import ScrollToTopButton from './ScrollToTopButton';
 import ServicesSection from './ServicesSection';
 import miniCarteImage from './assets/imagesEtLogo/images/miniCarte.png';
 
-
 function Accueil() {
   return (
     <div>
+      {/* Header Component */}
       <Header />
+
+      {/* Carousel Component */}
       <CarouselComponent />
+
+      {/* Billetterie Button */}
       <div>
         <Link to="/billetterie" className="btn bgTurquoise h4 btn-lg orange" title="Voir la billetterie" id="laBilletterie">
           <strong>LA BILLETTERIE</strong>
         </Link>
       </div>
+
+      {/* Artists Section */}
       <div className="container">
-  <div className="row">
-    <div className="col-12">
-      <h2 className="text-center pink bold-title mb-5">Les Artistes</h2>
-    </div>
-  </div>
-  <div className="row">
-    <div className="col-12">
-      <ArtistCard/>
-    </div>
-  </div>
-</div>
-      <ServicesSection />
-      <div>
-      <div className="row">
-        <div className="col-12 col-md-4 ps-5 ms-5">
-          <div className="newsletter-section">
-          <h3>Inscrivez-vous à notre newsletter</h3>
-          <Link to="/newsletter">
-            <button type="button">S'abonner <MdEmail /></button>
-          </Link>
+        <div className="row">
+          <div className="col-12">
+            <h2 className="text-center pink bold-title mb-5">Les Artistes</h2>
           </div>
         </div>
-        <div className="col-12 col-md-7"style={{ textAlign: 'center' }} >
-          <h3>Carte Interactive</h3>
+        <div className="row">
+          <div className="col-12">
+            {/* ArtistCard Component */}
+            <ArtistCard />
+          </div>
+        </div>
+      </div>
+
+      {/* Services Section */}
+      <ServicesSection />
+
+      {/* Newsletter and Interactive Map Section */}
+<div className="row mx-auto">
+  {/* Newsletter Section */}
+  <div className="col-12 col-md-4 ps-5 mx-auto">
+    <Card>
+      <Card.Body>
+        <Card.Title className='pink fw-bold'>Inscrivez-vous à notre newsletter</Card.Title>
+        <Card.Text>
+          <p>Pour avoir toutes les dernières informations, abonnez-vous!</p>
+          <Link to="/newsletter">
+            <Button className='bgPink bgNews'>S'abonner <MdEmail /></Button>
+          </Link>
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  </div>
+
+  {/* Interactive Map Section */}
+  <div className="col-12 col-md-4 mx-auto">
+    <Card>
+      <Card.Body>
+        <Card.Title className='pink fw-bold'>Carte Interactive</Card.Title>
+        <Card.Text>
           <p>Découvrez le plan du festival sur la carte interactive.</p>
           <img
-                    src={miniCarteImage}
-                    alt="présentation de la carte interactive"
-                    style={{ maxWidth: '(20%', height: 'auto', display: 'block', margin: '0 auto' }}
-                />
+            src={miniCarteImage}
+            alt="présentation de la carte interactive"
+            style={{ width: '100%', height: 'auto', maxWidth: '500px', display: 'block', margin: '0 auto' }}
+          />
           <p>
             Consultez la <a href="/myMap">carte interactive</a> pour plus de détails.
           </p>
-          {/* Autres éléments de votre page */}
-         
-        </div>
-      </div>
-    </div>
-     
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  </div>
+
+  {/* Troisième bloc avec le composant FestivalCountdown */}
+  <div className="col-12 col-md-4 mx-auto">
+        <Card>
+          <Card.Body>
+            <Card.Title className='pink fw-bold'>Compte à rebours du Festival</Card.Title>
+            <Card.Text>
+              {/* Intégrez le composant FestivalCountdown */}
+              <FestivalCountdown />
+            </Card.Text>
+          </Card.Body>
+        </Card>
+  </div>
+</div>
+{/* Fin de la section */}
+      {/* Footer Component */}
       <Footer />
-      {/* Ajoutez le bouton de retour en haut de la page */}
+
+      {/* ScrollToTopButton Component */}
       <ScrollToTopButton />
     </div>
   );
 }
 
 export default Accueil;
+
