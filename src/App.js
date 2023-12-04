17d81c874361a1fdbae8caf86a7c3ead0a5f19ef
wendy1976 <caroline.ferru@free.fr>
2023-12-04 14:10:23 +0100

    Refactor indentation and add comments in Accueil component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+// Importer le CSS de Bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
+// Importer le bundle JavaScript de Bootstrap
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
@@ -6,14 +8,14 @@ import AppRoutes from './routes';
 
 function App() {
   return (
-    
-      <div>
-        <Router>
-          <AppRoutes />
-        </Router>
-      </div>
-      
-    
+    // Utiliser un fragment pour envelopper le contenu JSX
+    <>
+      {/* Router pour permettre la navigation dans l'application */}
+      <Router>
+        {/* Le composant AppRoutes contient les routes définies */}
+        <AppRoutes />
+      </Router>
+    </>
   );
 }
 
