72aa608ab50f709a547c92a5130cbbd3ccba5afc
wendy1976 <caroline.ferru@free.fr>
2023-12-04 15:42:45 +0100

    refactor: reindent code in Infos.js

--- src/Infos.js
+++ src/Infos.js
@@ -1,9 +1,13 @@
 import { faBus, faCampground, faCar, faExclamationCircle, faGlassCheers, faHotel, faLaptop, faParking, faStore, faTrain, faUtensils } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+
+// Importation des composants nécessaires
 import Faq from './Faq';
 import Layout from './Layout';
 import ScrollToTopButton from './ScrollToTopButton';
+
+// Importation des images
 import InfosImage from './assets/imagesEtLogo/images/Infos.jpg';
 import accessibiliteImage from './assets/imagesEtLogo/images/accessibilite.jpg';
 import barImage from './assets/imagesEtLogo/images/bar.jpg';
@@ -17,6 +21,7 @@ function Infos() {
     return (
         <Layout>
             <div className='text-center bgWhite'>
+                {/* En-tête de la page */}
                 <h1 className="text-center pink mb-0 mt-3 pt-0 pb-5">Informations pratiques et FAQ</h1>
                 <img
                     src={InfosImage}
@@ -24,6 +29,7 @@ function Infos() {
                     style={{ maxWidth: '70%', height: 'auto', display: 'block', margin: '0 auto' }}
                 />
                 
+                {/* Liste des sections de la page avec des liens vers chacune */}
                 <ul style={{ listStyle: 'none', padding: 0 }}>
                     <li><a href="#section1">Transport</a></li>
                     <li><a href="#section2">Logement</a></li>
@@ -38,55 +44,60 @@ function Infos() {
             <section className='text-center bgWhite' id="section1">
                 <h2 className='text-center pink mb-0 mt-3 pt-0 pb-5'>Transport</h2>
                 <img
-                src={transportImage}
-                alt="Transport"
-                style={{ display: 'block', margin: '0 auto', maxWidth: '70%', height: 'auto' }}
+                    src={transportImage}
+                    alt="Transport"
+                    style={{ display: 'block', margin: '0 auto', maxWidth: '70%', height: 'auto' }}
                 />
                 <div className="pinkSeparator"></div> 
                 <div className="mx-md-5">
-                <h3 className='pink mt-5'>
-                    <FontAwesomeIcon icon={faTrain} className="mr-3 animate__animated animate__pulse animate__slow	2s animate__infinite	infinite" /> En train
-                </h3>   
+                    {/* Sous-section En train */}
+                    <h3 className='pink mt-5'>
+                        <FontAwesomeIcon icon={faTrain} className="mr-3 animate__animated animate__pulse animate__slow	2s animate__infinite	infinite" /> En train
+                    </h3>   
                     <p>
                         Le festival est accessible en train depuis la gare de Paris Montparnasse, et toutes autres villes. Vous pouvez cliquer sur le bouton ci-dessous pour accéder au site de la SNCF et réserver votre billet.
                     </p>
                     <a href="https://www.sncf-connect.com/" target="_blank" rel="noopener noreferrer">
-                    <button className="myButton mb-3">En train &#10132;</button>
+                        <button className="myButton mb-3">En train &#10132;</button>
                     </a>
                 </div>
 
+                {/* Sous-section En bus */}
                 <div className="mx-md-5">
-                <h3 className='pink mt-3'>
-                    <FontAwesomeIcon icon={faBus} className="mr-3 animate__animated animate__pulse animate__slow	2s animate__infinite	infinite" /> En bus
-                </h3>
+                    <h3 className='pink mt-3'>
+                        <FontAwesomeIcon icon={faBus} className="mr-3 animate__animated animate__pulse animate__slow	2s animate__infinite	infinite" /> En bus
+                    </h3>
                     <p>
                         Le festival est accessible en bus depuis la gare de Paris Montparnasse, et autres arrêts. Vous pouvez cliquer sur le bouton ci-dessous pour accéder au site de la RATP.
                     </p>
                     <a href="https://www.ratp.fr/plan-bus" target="_blank" rel="noopener noreferrer">
-                    <button className="myButton mb-3">En bus &#10132;</button>
+                        <button className="myButton mb-3">En bus &#10132;</button>
                     </a>
                 </div>
 
+                {/* Sous-section En voiture */}
                 <div className="mx-md-5">
-                <h3 className='pink mt-3'>
-                    <FontAwesomeIcon icon={faCar} className="mr-3 animate__animated animate__pulse animate__slow	2s animate__infinite	infinite" /> En voiture
-                </h3>
+                    <h3 className='pink mt-3'>
+                        <FontAwesomeIcon icon={faCar} className="mr-3 animate__animated animate__pulse animate__slow	2s animate__infinite	infinite" /> En voiture
+                    </h3>
                     <p>
                         Vous pouvez cliquer sur le bouton ci-dessous pour accéder au site de Google Maps et calculer votre itinéraire.
                     </p>
                     <a href="https://www.google.fr/maps/" target="_blank" rel="noopener noreferrer">
-                    <button className="myButton mb-3">En voiture &#10132;</button>
+                        <button className="myButton mb-3">En voiture &#10132;</button>
                     </a>
                 </div>
+
+                {/* Sous-section Les parkings à proximité */}
                 <div className="mx-md-5">
-                <h3 className='pink mt-3'>
-                    <FontAwesomeIcon icon={faParking} className="mr-3 animate__animated animate__pulse animate__slow	2s animate__infinite	infinite" /> Les parkings à proximité
-                </h3>
+                    <h3 className='pink mt-3'>
+                        <FontAwesomeIcon icon={faParking} className="mr-3 animate__animated animate__pulse animate__slow	2s animate__infinite	infinite" /> Les parkings à proximité
+                    </h3>
                     <p>
                         Le festival met à disposition un parking gratuit pour tous les festivaliers ayant acheté le PASS 2 Jours ou 3 Jours. Vous pouvez cliquer sur le bouton ci-dessous pour accéder au site de la ville de Paris et trouver un parking à proximité.
                     </p>
                     <a href="https://www.paris.fr/pages/trouver-un-parking-2270" target="_blank" rel="noopener noreferrer">
-                    <button className="myButton mb-5">Les parkings &#10132;</button>
+                        <button className="myButton mb-5">Les parkings &#10132;</button>
                     </a>
                 </div>
             </section>
@@ -95,15 +106,16 @@ function Infos() {
             <section className='text-center bgWhite' id="section2">
                 <h2 className='text-center pink mb-0 mt-3 pt-0 pb-5'>Logement</h2>
                 <img
-                src={campingImage}
-                alt="Camping"
-                style={{ display: 'block', margin: '0 auto', maxWidth: '70%', height: 'auto' }}
+                    src={campingImage}
+                    alt="Camping"
+                    style={{ display: 'block', margin: '0 auto', maxWidth: '70%', height: 'auto' }}
                 />
                 <div className="pinkSeparator"></div>
                 <div className='mx-md-5'>
-                <h3 className='pink mt-5'>
-                    <FontAwesomeIcon icon={faCampground} className="mr-3 animate__animated animate__swing animate__slow	2s animate__infinite	infinite" /> Le camping du festival
-                </h3>
+                    {/* Sous-section Le camping du festival */}
+                    <h3 className='pink mt-5'>
+                        <FontAwesomeIcon icon={faCampground} className="mr-3 animate__animated animate__swing animate__slow	2s animate__infinite	infinite" /> Le camping du festival
+                    </h3>
                     <h4 className='pink mt-5'>Le camping est gratuit pour tous les festivaliers ayant acheté le PASS 2 Jours ou 3 Jours.</h4>
                     <p>Le camping est monté pour l’occasion, et vous permettra de planter votre tente et de faire de nouvelles rencontres. Il vous propose les services suivants:</p>
                     <ul style={{ listStyle: 'none', padding: 0, fontWeight: 'bold' }}>
@@ -116,15 +128,16 @@ function Infos() {
                 </div>
                 
                 <img
-                src={hotelImage}
-                alt="Hôtel"
-                style={{ display: 'block', margin: '0 auto', maxWidth: '70%', height: 'auto' }}
+                    src={hotelImage}
+                    alt="Hôtel"
+                    style={{ display: 'block', margin: '0 auto', maxWidth: '70%', height: 'auto' }}
                 />
                 <div className="pinkSeparator"></div>
                 <div>
-                <h3 className='pink mt-5'>
-                    <FontAwesomeIcon icon={faHotel} className="mr-3 animate__animated animate__swing animate__slow	2s animate__infinite	infinite" /> Les hôtels aux alentours
-                </h3>
+                    {/* Sous-section Les hôtels aux alentours */}
+                    <h3 className='pink mt-5'>
+                        <FontAwesomeIcon icon={faHotel} className="mr-3 animate__animated animate__swing animate__slow	2s animate__infinite	infinite" /> Les hôtels aux alentours
+                    </h3>
                     <p>Si vous préférez un certain confort, nous vous proposons une liste d’hôtels aux alentours du festival:</p>
                     <ul style={{ listStyle: 'none', padding: 0 }}>
                         <li>HOTEL IBIS</li>
@@ -138,13 +151,14 @@ function Infos() {
 
             {/* Section Restauration & Bar */}
             <section className='text-center bgWhite' id="section3">
-            <h2 className='pink mt-3'>
-                <FontAwesomeIcon icon={faUtensils} className="mr-3 animate__animated animate__swing animate__slow	2s animate__infinite	infinite" /> Les restaurants
-            </h2>
+                {/* Sous-section Les restaurants */}
+                <h2 className='pink mt-3'>
+                    <FontAwesomeIcon icon={faUtensils} className="mr-3 animate__animated animate__swing animate__slow	2s animate__infinite	infinite" /> Les restaurants
+                </h2>
                 <img
-                src={restaurationImage}
-                alt="Restauration"
-                style={{ display: 'block', margin: '0 auto', maxWidth: '70%', height: 'auto' }}
+                    src={restaurationImage}
+                    alt="Restauration"
+                    style={{ display: 'block', margin: '0 auto', maxWidth: '70%', height: 'auto' }}
                 />
                 <div className="pinkSeparator"></div>
                 <div className='mx-md-5'>
@@ -154,30 +168,31 @@ function Infos() {
                     <p>Dans une ambiance très conviviale, « La Charrette de papy » vous propose une large carte de sandwichs, des hamburgers, des pizzas, des frites, des boissons et des desserts.</p>
                    
                     <h4 className='pink mt-5'>Restauration du monde: « L’Epicurium »</h4>
-                    <p>« L’Epicurium » vous propose une très grande diversité de cuisine du monde: asiatique (sushis, nems, brochettes, rouleaux de printemps entre autres),végétarienne (salades, pokébowl…), espagnole (tapas, paella…).</p>
+                    <p>« L’Epicurium » vous propose une très grande diversité de cuisine du monde: asiatique (sushis, nems, brochettes, rouleaux de printemps entre autres), végétarienne (salades, pokébowl…), espagnole (tapas, paella…).</p>
                     
                     <h4 className='pink mt-5'>Restauration du terroir: « La cuisine des bichettes »</h4>
                     <p>Dans une ambiance chaleureuse, « La cuisine des bichettes », vous propose une très grande variété de plats traditionnels (poulet basquaise, choucroute…), de planchettes (fromages, charcuterie), des huîtres, de la mouclade et autres fruits de mer, de salades, de quiches, et de crêpes (galettes bretonnes), et de succulents desserts (profiteroles, tartes, glaces, crème brûlée…)</p>
                 </div>
+
+                {/* Sous-section Le bar du festival */}
                 <h3 className='pink mt-5'>
                     <FontAwesomeIcon icon={faGlassCheers} className="mr-3 animate__animated animate__swing animate__slow	2s animate__infinite	infinite" /> Le bar du festival
                 </h3>
                 <img
-                src={barImage}
-                alt="Bar Nation Sounds"
-                style={{ display: 'block', margin: '0 auto', maxWidth: '70%', height: 'auto' }}
+                    src={barImage}
+                    alt="Bar Nation Sounds"
+                    style={{ display: 'block', margin: '0 auto', maxWidth: '70%', height: 'auto' }}
                 />
                 <div className="pinkSeparator"></div>
-
             </section>
 
             {/* Section Accessibilité */}
             <section className='text-center bgWhite' id="section4">
                 <h2 className='text-center pink mb-0 mt-3 pt-0 pb-5'>Accessibilité</h2>
                 <img
-                src={accessibiliteImage}
-                alt="Accessibilité"
-                style={{ display: 'block', margin: '0 auto', maxWidth: '70%', height: 'auto' }}
+                    src={accessibiliteImage}
+                    alt="Accessibilité"
+                    style={{ display: 'block', margin: '0 auto', maxWidth: '70%', height: 'auto' }}
                 />
                 <div className="pinkSeparator"></div>
                 <div className='mx-md-5' >
@@ -195,19 +210,27 @@ function Infos() {
             <section className='text-center bgWhite' id="section5">
                 <h2 className='text-center pink mb-0 mt-3 pt-0 pb-5' >Infos vente</h2>
                 <img
-                src={ticketImage}
-                alt="Ticket"
-                style={{ display: 'block', margin: '0 auto', maxWidth: '70%', height: 'auto' }}
+                    src={ticketImage}
+                    alt="Ticket"
+                    style={{ display: 'block', margin: '0 auto', maxWidth: '70%', height: 'auto' }}
                 />
                 <div className="pinkSeparator"></div>
                 <div>
                     <div>
-                        <FontAwesomeIcon icon={faLaptop} className='pink mr-3 animate__animated animate__swing animate__slow	2s animate__infinite	infinite ' style={{ fontSize: '3em' }} />
+                        <FontAwesomeIcon
+                            icon={faLaptop}
+                            className='pink mr-3 animate__animated animate__swing animate__slow 2s animate__infinite infinite'
+                            style={{ fontSize: '3em' }}
+                        />
                     </div>
                     <h3 className='pink mt-5'>En ligne</h3>
                     <p>Pour acheter votre billet, rendez-vous sur notre BILLETTERIE EN LIGNE</p>
                     <div>
-                        <FontAwesomeIcon icon={faStore} className='pink mr-3 animate__animated animate__swing animate__slow	2s animate__infinite	infinite' style={{ fontSize: '3em' }} />
+                        <FontAwesomeIcon
+                            icon={faStore}
+                            className='pink mr-3 animate__animated animate__swing animate__slow 2s animate__infinite infinite'
+                            style={{ fontSize: '3em' }}
+                        />
                     </div>
                     <h3 className='pink mt-5'>En magasin</h3>
                     <p>Liste des points de vente: Fmac, Leplerc, Ochamps, Culturo, Carrif</p>
@@ -217,12 +240,12 @@ function Infos() {
             {/* Section FAQ */}
             <section className='text-center bgWhite' id="section6">
                 <h2 className='text-center pink mb-0 mt-3 pt-0 pb-5'>FAQ</h2>
-            <Faq /> {/* Intégrez votre composant FAQ ici */}
+                <Faq /> {/* Intégrez votre composant FAQ ici */}
             </section>
-            {/* Ajoutez le bouton de retour en haut de la page */}
-        <ScrollToTopButton />
-        </Layout>
 
+            {/* Ajoutez le bouton de retour en haut de la page */}
+            <ScrollToTopButton />
+        </Layout>
     );
 }
 
